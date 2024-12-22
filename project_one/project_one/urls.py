08a296a2b2a7e882a2d6a191de9efd5604b8ae90
Thulani Mafalala <thulanism@gmail.com>
2024-12-22 19:35:07 +0200

    Local

--- project_one/project_one/urls.py
+++ project_one/project_one/urls.py
@@ -15,7 +15,6 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 
-#from django.conf.urls import url
 from django.contrib import admin
 from django.urls import path
 from first_app import views
