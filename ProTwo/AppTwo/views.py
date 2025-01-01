03125cb139073d7dc656b5b291950bbe6ef1ceae
Thulani Mafalala <thulanism@gmail.com>
2025-01-01 22:22:33 +0200

    Today's bit

--- ProTwo/AppTwo/views.py
+++ ProTwo/AppTwo/views.py
@@ -4,4 +4,4 @@ from django.http import HttpResponse
 # Create your views here.
 
 def index(response):
-    return HttpResponse("<em>This is my 2nd App!</em>" + "<div>This is a Div line.</div>")
+    return HttpResponse("<em>This is my 2nd App!</em>"+"<div>This is a Div line.</div>"+"<p>The main thrust of the story is in the intepretation you choose to adopt as the meaning.</p>")
