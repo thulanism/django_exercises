a817e2f52e1de74b6fed9233e0339367033b0488
Thulani Mafalala <thulanism@gmail.com>
2025-01-14 21:58:58 +0200

    Test passed

--- ProTwo/AppTwo/views.py
+++ ProTwo/AppTwo/views.py
@@ -3,7 +3,9 @@ from django.http import HttpResponse
 
 # Create your views here.
 
+def index(request):
+    return HttpResponse("<em>This is my 2nd App!</em>")
+
 def help(request):
-    my_dict = {'help_me' : "I am here to help you."}
-    return render(request,'AppTwo/help.html',context=my_dict)
-    #return HttpResponse("<em>This is my 2nd App!</em>")
+    help_dict = {'help_me' : "I am here to help you."}
+    return render(request,'AppTwo/help.html',context=help_dict)
--- ProTwo/ProTwo/urls.py
+++ ProTwo/ProTwo/urls.py
@@ -16,9 +16,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.conf.urls import include
 from AppTwo import views
 
 urlpatterns = [
-    path('', views.help, name='help'),
+    path('', views.index, name='index'),
+    path('help/', include('AppTwo.urls')),
     path('admin/', admin.site.urls),
 ]
